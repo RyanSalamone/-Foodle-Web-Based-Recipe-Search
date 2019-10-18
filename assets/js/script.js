7a471bd3f52dc56e6a298a99730bc64bf21cf423
Richard Germaine <rlgerma92@outlook.com>
2019-10-17 21:31:40 -0600

    idk

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,47 @@
-
-
-object = {
-    searchTerms: String,
-    numberOfPages: Number,
-    sortBy: Boolean, 
-}
-
-function executeFunction(data) {
-    var pagesCount = data.numberOfPages;
-    var searchList = data.searchTerms;
-    var sortedBy = data.sortBy;
-    var apiKey = c746a1cb0d80b9ef3f8ab026e25ff5b4;
-}
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-
-    for (var i = 0; i <pagesCount; i++){
-        console.log(response.response);
-        title = response.response.title;
-        sourceUrl = response.response.source_url;
-        console.log(heading);
-         console.log(webUrl);
-    }
-
-});
-
-$(document).ready(function() {
-    $(document).on('click','button', function(event) {
-    //  $(".dailyPlan").submit( function(event) {
-      event.preventDefault();
-      var searchTerms = $('#');
+  
+    $(document).ready(function(){
+        
+        var pagesCount = data.numberOfPages;
+        var searchList = data.searchTerms;
+        var sortedBy = data.sortBy;
+        var apiKey = "&c746a1cb0d80b9ef3f8ab026e25ff5b4";
+        
+        object = {
+            searchTerms: String,
+            numberOfPages: Number,
+            sortBy: Boolean, 
+        }
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            
+            for (var i = 0; i <pagesCount; i++){
+                console.log(response.response);
+                title = response.response.title;
+                sourceUrl = response.response.source_url;
+                console.log(heading);
+                console.log(webUrl);
+            }
+            
+        });
+    
+       $(document).on('click','button', function(event) {
+      //  $(".dailyPlan").submit( function(event) {
+        event.preventDefault();
+        var searchTerms = $('#');
       // need to make select box
-      var numRecord = $('#');
+        var numRecord = $('#');
       // input validation
     
       // validation here
-      var queryParamObj = {
+        var queryParamObj = {
         searchTerm: searchTerm.val(),
         numberOfRecords: numRecord.val(),
         startYr: startYr.val(),
         startYr: endYr.val(),
-      }
-      var resultData = executeQuery(queryParamObj);
+       }
+        var resultData = executeQuery(queryParamObj);
       // draw_results(resultData);
+      })
     });
-   }
